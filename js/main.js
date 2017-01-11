d46da7156295bbf552415f197da0f6d509c433cf
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-01-10 19:51:33 -0600

    clean up map js. add flyto

--- js/main.js
+++ js/main.js
@@ -30,56 +30,60 @@ Site = {
 
 Site.Map = {
   map,
+  coordinateArray: [],
 
   init: function() {
     var _this = this;
 
-    navigator.geolocation.getCurrentPosition(function(position) {
-      _this.initMap(position.coords.latitude, position.coords.longitude);
-    });
-  },
-
-  initMap: function(lat, long) {
-    var _this = this;
-
     _this.map = L.map('map', {
       dragging: false
-    }).setView([lat, long], 20);
+    }).setView([0, 0], 2);
+
+    _this.tileMap();
+
+    _this.flyToDevicePos();
+
+    _this.draw();
+  },
+
+  tileMap: function() {
+    var _this = this;
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
       maxZoom: 20,
       id: 'mapbox.streets'
     }).addTo(_this.map);
+  },
 
-    var coordinateArray = [];
+  flyToDevicePos: function() {
+    var _this = this;
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      _this.map.flyTo([position.coords.latitude, position.coords.longitude], 20, {
+        animate: true
+      });
+    });
+  },
+
+  draw: function() {
+    var _this = this;
 
     _this.map.on('mouseup', function() {
-
       _this.map.off("mousemove");
-
-      L.polyline(coordinateArray, {color: '#f94000'}).addTo(_this.map);
-
-      console.log(coordinateArray);
-
-      coordinateArray = [];
-
-      navigator.geolocation.getCurrentPosition(function(position) {
-        _this.map.panTo(new L.LatLng(position.coords.latitude, position.coords.longitude));
-      });
-     
-
+      L.polyline(_this.coordinateArray, {color: 'black'}).addTo(_this.map);
+      _this.coordinateArray = [];
     }).on('mousedown', function() {
-
       _this.map.on("mousemove", function(e) {
-        trackPoints(e)
+        _this.trackMouse(e)
       });
-
     });
-
-    function trackPoints(e) {
-      coordinateArray.push([e.latlng.lat, e.latlng.lng]);
-    }
   },
+
+  trackMouse: function(e) {
+    var _this = this;
+
+    _this.coordinateArray.push([e.latlng.lat, e.latlng.lng]);
+  }
 }
 
 jQuery(document).ready(function () {
